554eb5d8d3ba0de483a81b11fb175b64392573a6
aurora5090 <aurora5090@qq.com>
2025-12-29 19:54:05 +0800

    [chore] simplify fee workflow

---
+++ src/check/companyCheck.ts
@@ -0,0 +1,78 @@
+import axios from "axios";
+import { sameCheck } from "../agent/sameCheck";
+import { getPinyin } from "../pinyin";
+
+type CompanyCheckResult = {
+  value: null | string;
+  id: null | number;
+  checked: boolean;
+};
+
+export async function companyCheck(
+  name: string | null
+): Promise<CompanyCheckResult> {
+  let ret: CompanyCheckResult = {
+    value: null,
+    id: null,
+    checked: false,
+  };
+
+  if (name === null) return ret;
+
+  console.log(`开始检查 Company`);
+
+  const py = getPinyin(name);
+
+  // 第一层 mysql 按编辑距离，筛选 top 10
+  const some = (
+    await axios.get(
+      `http://111.229.188.40:3000/api/companies/name/search/${py}`
+    )
+  ).data.data as Record<string, any>;
+
+  // LLM 二次处理候选项，做语义判断
+  const checkResult = await sameCheck(
+    JSON.stringify(some),
+    `${name}
+        ${py}
+        `
+  );
+
+  if (checkResult === false) {
+    // 模型没有进行工具调用
+    ret.value = null;
+
+    ret.checked = false;
+
+    console.error(new Error("模型没有进行工具调用"));
+  } else {
+    const id = checkResult.args.id as null | number;
+
+    if (id === null) {
+      // 模型认为不存在匹配的对象
+      ret.value = null;
+
+      ret.checked = false;
+
+      console.log(`模型认为不存在匹配的 Company`);
+    } else {
+      // 存在匹配的公司名称
+
+      ret.id = id;
+
+      for (const key in some) {
+        if (!Object.hasOwn(some, key)) continue;
+
+        const element = some[key];
+
+        if (element.id === id) ret.value = element.name;
+      }
+
+      ret.checked = true;
+
+      console.log(`存在匹配的 Company: ${ret.value}`);
+    }
+  }
+
+  return ret;
+}
--- src/workflow/fee1/index.ts
+++ src/workflow/fee1/index.ts
@@ -7,6 +7,9 @@ import { getPinyin } from "../../pinyin";
 import { sameCheck } from "../../agent/sameCheck";
 import { feeSlot } from "../../agent/slot/feeSlot";
 import { feeSummary } from "../../agent/feeSummary";
+import { companyCheck } from "../../check/companyCheck";
+import { projectCheck } from "../../check/projectCheck";
+import { object } from "zod";
 
 const FlowID = 1;
 
@@ -28,88 +31,25 @@ async function check(talker: Talker<FlowData>) {
   const must: string[] = [];
   const optional: string[] = [];
 
-  async function makeCheck(
-    key: keyof Metadata,
-    description: string,
-    api: string
-  ) {
-    const aimData = data[key];
-
-    if (aimData.checked) return;
-
-    if (aimData.value !== null) {
-      console.log(`开始检查 ${key}`);
-
-      const py = getPinyin(aimData.value);
-
-      // 第一层 mysql 按编辑距离，筛选 top 10
-      const some = (await axios.get(`${api}${py}`)).data.data as Record<
-        string,
-        any
-      >;
-
-      // LLM 二次处理候选项，做语义判断
-      const checkResult = await sameCheck(
-        JSON.stringify(some),
-        `${aimData.value}
-        ${py}
-        `
-      );
-
-      if (checkResult === false) {
-        // 模型没有进行工具调用
-        aimData.value = null;
-
-        aimData.checked = false;
-
-        console.error(new Error("模型没有进行工具调用"));
-      } else {
-        const id = checkResult.args.id as null | number;
-
-        if (id === null) {
-          // 模型认为不存在匹配的对象
-          aimData.value = null;
-
-          aimData.checked = false;
-
-          console.log(`模型认为不存在匹配的 ${key}`);
-        } else {
-          // 存在匹配的公司名称
-
-          aimData.id = id;
-
-          for (const key in some) {
-            if (!Object.hasOwn(some, key)) continue;
-
-            const element = some[key];
-
-            if (element.id === id) aimData.value = element.name;
-          }
-
-          aimData.checked = true;
-
-          console.log(`存在匹配的 ${key}: ${aimData.value}`);
+  let chk1 = data.company.checked
+    ? Promise.resolve(true)
+    : companyCheck(data.company.value).then((res) => {
+        if (!res.checked) {
+          must.push("公司名称");
         }
-      }
-    }
+        Object.assign(data.company, res);
+      });
 
-    // 如果依然缺失，加入 must 数组
-    if (!aimData.checked) must.push(description);
-  }
+  let chk2 = data.project.checked
+    ? Promise.resolve(true)
+    : projectCheck(data.project.value).then((res) => {
+        if (!res.checked) {
+          must.push("缴费项目名称");
+        }
+        Object.assign(data.project, res);
+      });
 
-  const vecp: Promise<void>[] = [
-    makeCheck(
-      "company",
-      "公司名称",
-      "http://111.229.188.40:3000/api/companies/name/search/"
-    ),
-    makeCheck(
-      "project",
-      "缴费项目名称",
-      "http://111.229.188.40:3000/api/payment-items/name/search/"
-    ),
-  ];
-
+  const vecp: Promise<unknown>[] = [chk1, chk2];
   await Promise.all(vecp);
 
   if (data.phone.checked || data.owner.checked) {
@@ -191,7 +131,6 @@ async function visit(talker: Talker<FlowData>, message: string) {
     const cid = talker.inFlowData.metadata.company.id;
     const pid = talker.inFlowData.metadata.project.id;
 
-    // const res = String(await basicChat(message, talker));
     const some = (
       await axios.get(
         `http://111.229.188.40:3000/api/flow/feeFlow?companyId=${cid}&paymentItemId=${pid}`
